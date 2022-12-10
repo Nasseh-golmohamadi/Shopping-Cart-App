0a04bda7612c643912ec3127106f2ec1d10f6dd8
Nasseh-golmohamadi <nasehgolmohamadi@gmail.com>
2022-12-10 06:55:41 -0800

    Bordering

--- src/component/Total.jsx
+++ src/component/Total.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function ({ price }) {
   return (
     <div className="total">
-      <h1>Total-Price :{price}</h1>
+      <h1>
+        Total-Price :<br />
+        {price}
+      </h1>
     </div>
   );
 }
